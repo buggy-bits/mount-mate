91acdfbc71f997e94fd51988467ecace4838c56e
Sainag <saigadangi@gmail.com>
2025-06-09 02:40:38 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import RepoToWebContainer from './RepoToWebContainer'
+
+function App() {
+
+  return (
+    <>
+      <RepoToWebContainer />
+    </>
+  )
+}
+
+export default App
